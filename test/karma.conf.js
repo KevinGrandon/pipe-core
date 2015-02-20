4a33af6c484db594810632bfa196fba128a67898
Kevin Grandon <kevingrandon@yahoo.com>
2015-02-20 15:01:26 +0100

    Add unit testing framework

---
+++ test/karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+  config.set({
+    frameworks: ['mocha', 'sinon-chai'],
+    browsers: ['firefox_latest'],
+    client: {
+      captureConsole: true,
+      mocha: { 'ui': 'tdd' }
+    },
+    basePath: '../',
+
+    customLaunchers: {
+      firefox_latest: {
+        base: 'FirefoxNightly',
+        prefs: {
+          'dom.webcomponents.enabled': true
+        }
+      }
+    },
+
+    files: [
+      'pipe.js',
+      {
+        pattern: 'test/worker.js',
+        included: false
+      },
+      'test/test.js'
+    ]
+  });
+};
